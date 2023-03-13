7791f690a7e62c041fcde4efc294c1ec14c80a42
joelborellis <joelborellis@outlook.com>
2023-03-12 19:21:16 -0500

    css changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+/* scriting for bios */
+
+var coll = document.getElementsByClassName("bio-collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("act");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 (function($) {
 	'use strict';
 	
