480eb699dc50b2db629502f7ccf3c00bc66dba0b
joelborellis <joelborellis@outlook.com>
2023-03-14 11:48:01 -0500

    map addition

--- js/main.js
+++ js/main.js
@@ -15,6 +15,64 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+/* google maps js */
+
+function initMap() {
+	const map = new google.maps.Map(document.getElementById("map"), {
+	  zoom: 10,
+	  center: { lat: 30.42834, lng: -97.83689 },
+	});
+
+	setMarkers(map);
+  }
+
+  // Data for the markers consisting of a name, a LatLng and a zIndex for the
+  // order in which these markers should display on top of each other.
+  const locs = [
+	["North Austin", 30.46348, -97.79302, 2],
+	["Lake Travis", 30.32288, -97.95868, 1],
+  ];
+
+  function setMarkers(map) {
+	// Adds markers to the map.
+	// Marker sizes are expressed as a Size of X,Y where the origin of the image
+	// (0,0) is located in the top left of the image.
+	// Origins, anchor positions and coordinates of the marker increase in the X
+	// direction to the right and in the Y direction down.
+	const image = {
+	  url: "https://centraltexasanxiety.azurewebsites.net/images/siteflag.png",
+	  // This marker is 20 pixels wide by 32 pixels high.
+	  size: new google.maps.Size(20, 32),
+	  // The origin for this image is (0, 0).
+	  origin: new google.maps.Point(0, 0),
+	  // The anchor for this image is the base of the flagpole at (0, 32).
+	  anchor: new google.maps.Point(0, 32),
+	};
+	// Shapes define the clickable region of the icon. The type defines an HTML
+	// <area> element 'poly' which traces out a polygon as a series of X,Y points.
+	// The final coordinate closes the poly by connecting to the first coordinate.
+	const shape = {
+	  coords: [1, 1, 1, 20, 18, 20, 18, 1],
+	  type: "poly",
+	};
+
+	for (let i = 0; i < locs.length; i++) {
+	  const loc = locs[i];
+
+	  new google.maps.Marker({
+		position: { lat: loc[1], lng: loc[2] },
+		map,
+		icon: image,
+		shape: shape,
+		title: loc[0],
+		zIndex: loc[3],
+	  });
+	}
+  }
+
+  window.initMap = initMap;
+
+
 (function($) {
 	'use strict';
 	
